408f49f1552cba057b15f13b7c49d5df4bf34551
Ning Yap <ning.yap@gmail.com>
2013-11-28 11:19:59 -0500

    create while loop to execute replace_px

--- PxToEm.py
+++ PxToEm.py
@@ -8,7 +8,7 @@ def replace_px(css):
     # grabs 12
     px = int(re.search(r'\d+',px_s).group())
 
-    # grabs 16 (base) or sets default to 16
+    # grabs /16 (base) or sets default to 16
     if re.search(r'/\d+',css) and re.search(r'/\d+',css).group():
         base_s = re.search(r'/\d+',css).group()
         base = int(re.search(r'\d+',base_s).group())
@@ -21,12 +21,10 @@ def replace_px(css):
 
     # constructs comment /* 12/16 */, omits if comment is /* 16/16 */
     comment = "" if (base == px and base == 16) else " /* %s/%s */" % (px,base)
-    # comment = " /* %s/%s */" % (px,base)
 
     # generate new string with comment
     new_css = re.sub(r'\d+px',em,css,1)
-    new_css = re.sub(r'\s/\d+',"",new_css,1) + comment
-    return new_css
+    return re.sub(r'\s/\d+',"",new_css,1) + comment
 
 class PxToEmCommand(sublime_plugin.TextCommand):
 
@@ -38,25 +36,10 @@ class PxToEmCommand(sublime_plugin.TextCommand):
 
             if css.find('\n') != -1:
                 print "Multiple line selection not supported"
-                return
-            elif re.search(r'px.+px.+px.+px.+px',css):
-                print "Invalid CSS: too many (px)'s to process"
-                return
-            elif re.search(r'px.+px.+px.+px',css):
-                px_count = 4
-            elif re.search(r'px.+px.+px',css):
-                px_count = 3
-            elif re.search(r'px.+px',css):
-                px_count = 2
-            elif re.search(r'px',css):
-                px_count = 1
+            elif (re.search(r'px',css) == None):
+                print "No px found"
             else:
-                print "no px found"
-                return
+                while (re.search(r'px',css)):
+                    css = replace_px(css)
 
-            s = css
-            while (px_count > 0):
-                s = replace_px(s)
-                px_count -= 1
-
-            self.view.replace(edit, line, s)
+                self.view.replace(edit, line, css)
